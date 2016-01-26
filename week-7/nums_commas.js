2a6120a26f8e80418f509f8cc6f8bd09e570868d
Darrow87 <alexforgerii@gmail.com>
2016-01-25 22:59:04 -0500

    add reflection

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -10,3 +10,7 @@ function separate_comma(num){
    console.log(num)
 }
 
+// What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently? No I didn't except for needing to use the for loop. Maybe I should have since I didnt solve it.
+// What did you learn about iterating over arrays in JavaScript? I learned that you have to use a for loop and you seem to be referencing the index of arrays quite frequently.
+// What was different about solving this problem in JavaScript? Using the for loop
+// What built-in methods did you find to incorporate in your refactored solution? reverse, split, join.
