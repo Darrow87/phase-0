fce503be9d98ff97ca80b214ad6dfb4697f8bfcb
Darrow87 <alexforgerii@gmail.com>
2016-01-26 23:29:40 -0500

    add eloguent js

---
+++ week-7/eloquent.js
@@ -0,0 +1,36 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+var array = [1, 2, 3, 4]
+array.reverse()
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+var x = "#";
+for (var i = 1; i < 8; i++) {
+    console.log(x)
+    x = x + "#"
+}
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+function min(num1, num2){
+  return min(num1, num2)
+}
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+var me = {
+  name: "Alex",
+  age: "29",
+  food: "Pizza"
+}
