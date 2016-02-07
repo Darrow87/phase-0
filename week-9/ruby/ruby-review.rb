217961d99d6441587f60cb9139e6a1580661e43d
Darrow87 <alexforgerii@gmail.com>
2016-02-07 15:58:45 -0500

    ruby review

---
+++ week-9/ruby/ruby-review.rb
@@ -0,0 +1,74 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+# create a loop that will run fib sequence
+# stop loop when it is greater than or equal to num
+# return true if equal to num, false if greater than
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+sum = 0
+sum_fib = 1
+  until sum_fib >= num
+    sum_fib = sum_fib + sum
+    sum = sum_fib - sum
+  end
+
+  if sum_fib == num
+    return true
+  else
+    return false
+  end
+
+end
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+
+# Reflection
+# What concepts did you review in this challenge?
+# Looping with a counter
+# What is still confusing to you about Ruby?
+# I like ruby much better than JS, but I'm still a little shaky on classes and also reader/writer functions
+# What are you going to study to get more prepared for Phase 1?
+# Classes
+# Introduction to Inheritance
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Pseudocode
+
+
+
+
+# Initial Solution
+
+class GlobalCohort
+  #your code here
+
+end
+
+class LocalCohort
+  #your code here
+
+end
+
+
+# Refactored Solution
+
+
+
+
+
+# Reflection
